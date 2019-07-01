51bc9a9f22e3355299b2870ca0fe1299596382b5
Mikhail <popovms1207@gmail.com>
2019-07-01 16:57:20 +0300

    Добавьте файлы проекта.

---
+++ WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Models; 
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        pubsEntities ob = new pubsEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getdata()
+        {
+            List<view_titles> data = ob.view_titles.ToList<view_titles>();
+            return Json(new {data}, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Postdata(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Postdata()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
